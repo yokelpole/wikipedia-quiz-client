8c6840fc05aa381cd5dfc33b9831f7c7a22acdf6
Kyle Poole <poolekyle@gmail.com>
2020-05-06 01:27:23 -0700

    Switch to websockets

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -14,20 +14,20 @@
       <h2>{{ activeQuestion.topic }}</h2>
       <h3>{{ activeQuestion.section }}</h3>
       <p>{{ activeQuestion.question }}</p>
-      <div v-for="(val, index) in activeQuestion.answers" v-bind:key="val">
-        <button v-on:click="submitAnswer(index)" :disabled="answer">{{ val }}</button>
+      <div v-for="(val) in activeQuestion.answers" v-bind:key="val">
+        <button v-on:click="submitAnswer(val)" :disabled="answer">{{ val }}</button>
       </div>
       <div>
-        <h2 v-if="correctAnswer === answer">CORRECT!</h2>
-        <div v-if="answer !== undefined && correctAnswer !== answer">
+        <h2 v-if="userIsCorrect === true">CORRECT!</h2>
+        <div v-if="userIsCorrect === false">
           <h2>WRONG!</h2>
-          <p>The correct answer is {{ activeQuestion.correct_answer }}</p>
+          <p>The correct answer is {{ correctAnswer }}</p>
         </div>
       </div>
       <div>
         <h5>{{ countdownValue }}</h5>
       </div>
-      <ul v-for="player in players" v-bind:key="player.id">
+      <ul v-for="player in orderedScoreboard" v-bind:key="player.id">
         <li>{{ player.name }} {{ player.score }}</li>
       </ul>
     </div>
@@ -39,118 +39,84 @@ export default {
   name: "Quiz",
   data: () => ({
     activeQuestion: {},
+    activeTimer: undefined,
     answer: undefined,
     correctAnswer: undefined,
     countdownValue: 0,
     playerName: undefined,
     playerId: undefined,
-    players: [],
+    userIsCorrect: undefined,
+    websocket: undefined,
+    players: {}
   }),
   created: async function() {
-    const setQuestion = (json) => {
+    const setQuestion = json => {
       this.answer = undefined;
-      json.answers.forEach((answer, i) => {
-        if (answer === json.correct_answer) this.correctAnswer = i;
-      });
+      this.userIsCorrect = undefined;
       json.topic = json.topic.replace(/_/g, " ");
       json.section = json.section.replace(/_/g, " ");
       this.activeQuestion = json;
-     }
+    };
 
-    const startCountdownTimer = () => {
-      setInterval(() => {
-        const newValue = parseFloat(this.countdownValue - 0.1).toFixed(1);
-        if (newValue > 0) {
-          this.countdownValue = newValue;
-        }
-      }, 100);
-    }
-
-    const getQuestionData = async () => {
-      const questionResp = await fetch(
-        "http://localhost:8090/get_current_question"
-      );
-      const json = await questionResp.json();
-      const activeQuestionStartTime = new Date(
-        json["active_question_start_time"]
-      );
-      const questionTime = json["question_time"];
+    const startCountdownTimer = finishTime => {
       const currentTime = new Date(
         new Date().toLocaleString("en-US", { timeZone: "UTC" })
       );
-      activeQuestionStartTime.setSeconds(
-        activeQuestionStartTime.getSeconds() + questionTime
-      );
+      this.countdownValue = (new Date(finishTime) - currentTime) / 1000;
+      clearInterval(this.activeTimer);
+      this.activeTimer = setInterval(() => {
+        const newValue = parseFloat(this.countdownValue - 0.01).toFixed(2);
+        if (newValue > 0) {
+          this.countdownValue = newValue;
+        }
+      }, 10);
+    };
 
-      return {
-        json,
-        currentTime,
-        activeQuestionStartTime
-      };
-    }
+    this.websocket = new WebSocket("ws://127.0.0.1:8100");
+    this.websocket.onmessage = msg => {
+      const data = JSON.parse(msg.data);
 
-    const fetchNextQuestion = time => {
-      // TODO: Add debounce for when server isn't updated quite yet or in an error condition.
-      setTimeout(async () => {
-        const {
-          json,
-          activeQuestionStartTime,
-          currentTime
-        } = await getQuestionData();
-        this.players = json.players.sort((a, b) => b.score - a.score);
-        setQuestion(json);
-        this.countdownValue = json.question_time;
-        fetchNextQuestion(
-          activeQuestionStartTime.getTime() - currentTime.getTime()
-        );
-      }, time);
-    }
-
-    const {
-      json,
-      activeQuestionStartTime,
-      currentTime
-    } = await getQuestionData();
-
-    this.players = json.players.sort((a, b) => b.score - a.score);
-    this.countdownValue = (activeQuestionStartTime - currentTime) / 1000;
-    startCountdownTimer();
-    setQuestion(json);
-    fetchNextQuestion(
-      activeQuestionStartTime.getTime() - currentTime.getTime()
-    );
+      if (data.type === "question_changed") {
+        setQuestion(data.value.question);
+        startCountdownTimer(data.value.finish_time);
+      }
+      if (data.type === "player_registered") this.playerId = data.value;
+      if (data.type === "leaderboard_updated") this.players = data.value;
+      if (data.type === "correct_answer") {
+        this.correctAnswer = data.value;
+        this.userIsCorrect = this.correctAnswer === this.answer;
+      }
+    };
   },
   methods: {
     submitName: async function(e) {
       e.preventDefault();
-      const playerResp = await fetch("http://localhost:8090/join_game", {
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Request-Headers": "x-requested-with"
-        },
-        body: JSON.stringify({ name: this.playerName })
-      });
-      const json = await playerResp.json();
-      this.playerId = json.ID;
-      this.$emit("playerActive", json);
-    },
-    submitAnswer: async function (index) {
-      this.answer = index;
-
-      fetch("http://localhost:8090/answer_question", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-        // FIXME: really shouldn't trust the client to say the answer is right...
-        body: JSON.stringify({
-          name: this.playerName,
-          correct: this.correctAnswer === this.answer
+      await this.websocket.send(
+        JSON.stringify({
+          type: "register_player",
+          value: {
+            name: this.playerName
+          }
         })
-      });
+      );
+    },
+    submitAnswer: async function(index) {
+      this.answer = index;
+      await this.websocket.send(
+        JSON.stringify({
+          type: "player_answer",
+          value: {
+            player_id: this.playerId,
+            answer: index
+          }
+        })
+      );
+    },
+  },
+  computed: {
+    orderedScoreboard: function () {
+      const playerMap = Object.entries(this.players).map(([, val]) => val);
+      return playerMap.sort((a, b) => b.score > a.score);
     }
   }
 };
